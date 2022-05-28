41a6c5f544d1ad11586732153eff490ff77cec9d
Judeous <judeous@tutanota.com>
2022-05-28 10:47:54 -0500

    Health and Respawning
    If a player dies, then it will respawn at a passed in transform
    If something else dies, then the GameObject will be destroyed

---
+++ Assets/Scripts/Gameplay/HealthBehavior.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehavior : MonoBehaviour
+{
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _maxHealth = 20;
+
+    [SerializeField] private bool _isPlayer = false;
+    private PlayerManagerBehavior _playerManager;
+
+    [SerializeField] private float _invincibilityTime = 10;
+    private float _timeSinceDamaged = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+
+        //Store a PlayerManager component if isPlayer is true
+        if (_isPlayer)
+        {
+            _playerManager = GetComponent<PlayerManagerBehavior>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeSinceDamaged += Time.deltaTime;
+    }
+
+    public void TakeDamage(int value = 1)
+    { 
+        if (_timeSinceDamaged > _invincibilityTime)
+        {
+            //Lower health
+            _currentHealth -= value;
+            _timeSinceDamaged = 0;
+
+            //If health has dropped to or below zero
+            if (_currentHealth <= 0)
+            {
+                if (_isPlayer)
+                {
+                    //Respawn the player with the PlayerManager component
+                    _playerManager.Respawn();
+                    //Restore some health to the player
+                    Heal(10, false, true);
+                }
+                else
+                {
+                    //May replace with making it flee, depending on whether this is an enemy or not and the artist wants it to flee
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    public void Heal(int value = 5, bool allowOverHeal = false, bool healHalfIfPossible = false)
+    {
+        //Calculate half of the player's missing health
+        int halfOfMissingHealth = (_maxHealth - _currentHealth) / 2;
+
+        //If half the missing health is greater than the passed in healing amount
+        if (healHalfIfPossible && halfOfMissingHealth > value)
+        {
+            //Add half of the missing health
+            _currentHealth += halfOfMissingHealth;
+            return;
+        }
+        //Otherwise, add the passed in heal amount
+        _currentHealth += value;
+
+        //If this Heal isn't allowed to overheal and the Heal surpassed the max health
+        if (!allowOverHeal && _currentHealth > _maxHealth)
+        {
+            //Clamp the health to the maximum
+            _currentHealth = _maxHealth;
+        }
+    }
+}
