aeb5dfaaf2b8d6ec58a831c49d3d44dbf4fabe3c
Judeous <judeous@tutanota.com>
2022-05-29 10:50:39 -0500

    Created SlashSpawnerBehavior, SwordSlashBehavior, and  aslash prefab
    If the player clicks , a sword slash will be created and move quickly towards where the click hit

---
+++ Assets/Scripts/Gameplay/SlashSpawnerBehavior.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class SlashSpawnerBehavior : MonoBehaviour
+{
+    /// <summary>
+    /// The GameObject that will be spawned by the Fire function. Needs to have SwordSlashBehavior attached
+    /// </summary>
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private float _duration = 0.25f;
+    [SerializeField] private float _speed = 0.4f;
+
+    [SerializeField] private float _cooldown = 0.25f;
+    [SerializeField] private float _timeSinceFire = 10;
+    /// <summary>
+    /// The GameObject that will not be hit by the slash
+    /// </summary>
+    [SerializeField] private GameObject _owner;
+    /// <summary>
+    /// The Camera to check a raycast hit from to determine the direction to send the slash in
+    /// </summary>
+    [SerializeField] private Camera _camera;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        _timeSinceFire += Time.deltaTime;
+    }
+
+    public void TryFire()
+    {
+        if (_timeSinceFire > _cooldown)
+        {
+            Fire();
+        }
+    }
+
+    public void Fire()
+    {
+        //Get a raycasthit from the camera based on the mouse position
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        Vector3 direction;
+        Vector3 force;
+        //If the cursor hit something
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            //Get a vector from the current position to the hit position
+            direction = new Vector3(hit.point.x - transform.position.x, 0, hit.point.z - transform.position.z).normalized;
+        }
+        else
+        {
+            //Otherwise, make the direction forwards
+            direction = transform.forward;
+        }
+        //Calculate the force to give the slash
+        force = direction * _speed;
+
+        //Create an instance of a Slash
+        GameObject slash = Instantiate(_projectile, transform.position, transform.rotation);
+        
+        //Get the slashbehavior component
+        if (slash.TryGetComponent<SwordSlashBehavior>(out var slashBehavior))
+        {
+            //Get the rigidbody component
+            if (slash.TryGetComponent(out Rigidbody slashRigidbody))
+            {
+                //Add the force and set the owner and duration
+                slashRigidbody.AddForce(force, ForceMode.Impulse);
+                slashBehavior.Owner = _owner;
+                slashBehavior.Duration = _duration;
+                _timeSinceFire = 0;
+            }
+
+        }
+    }
+}
--- Assets/Scripts/Input/InputEventBehavior.cs
+++ Assets/Scripts/Input/InputEventBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InputEventBehavior : MonoBehaviour
 {
     private PlayerMovementBehavior _playerMovementBehavior;
+    [SerializeField] SlashSpawnerBehavior _splashSpawnerBehavior;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +20,16 @@ public class InputEventBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get movement input
         Vector3 movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
+        //Normalize movement
         if (movementInput.magnitude > 1) movementInput.Normalize();
-
+        //Actually move
         _playerMovementBehavior.Move(movementInput);
+
+        //Get fire input
+        float swordslash = Input.GetAxis("Fire1");
+        if (swordslash != 0)
+        { _splashSpawnerBehavior.TryFire(); }
     }
 }
